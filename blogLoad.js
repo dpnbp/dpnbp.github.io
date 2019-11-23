17a71a1e6b4b0411614a983d858cfec82bf25f71
Dan P <me@dipanp.com>
2019-11-23 14:53:21 -0500

    blog good enough

---
+++ blogLoad.js
@@ -0,0 +1,9 @@
+//blogLoad.js
+
+var blogLinks = document.getElementsByClass("blogLink");
+
+for(let i=0; i<blogLinks.length; i++){
+	let artTitle = document.getElementByID("artTitle");
+	let linkText = blogLinks[i].textContent;
+
+}
